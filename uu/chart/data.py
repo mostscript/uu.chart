fb72efc998a1881b06f9dfe6738801e2e429e0ed
Sean Upton <sdupton@gmail.com>
2011-09-13 10:58:50 -0600

    Added data.py with classes for data point representation... These will be used by views and content classes, but are transient objects created at runtime from data to be stored or queried from sources like (but not limited to) time data sequence objects containing a data grid field.

---
+++ uu/chart/data.py
@@ -0,0 +1,42 @@
+import datetime
+
+from zope.interface import implements
+
+from uu.chart.interfaces import INamedDataPoint, ITimeSeriesDataPoint
+
+
+class BaseDataPoint(object):
+    def __init__(self, value, note=None, uri=None):
+        self.value = value
+        self.note = note
+        self.uri = uri
+    
+    def identity(self):
+        raise NotImplementedError('base class does not provide')
+
+
+class NamedDataPoint(BaseDataPoint):
+    implements(INamedDataPoint)
+    
+    def __init__(self, name, value, note=None, uri=None):
+        self.name = name
+        super(NamedDataPoint, self).__init__(value, note, uri)
+
+    def identity(self):
+        return self.name
+
+
+class TimeSeriesDataPoint(BaseDataPoint):
+    implements(ITimeSeriesDataPoint)
+    
+    def __init__(self, date, value, note=None, uri=None):
+        if isinstance(date, datetime.datetime):
+           date = date.date()
+        if not isinstance(date, datetime.date):
+            raise ValueError('date must be datetime.date object')
+        self.date = date
+        super(NamedDataPoint, self).__init__(value, note, uri)
+
+    def identity(self):
+        return self.date
+
