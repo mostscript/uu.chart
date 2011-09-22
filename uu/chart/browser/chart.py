2b1ebf59aeabccfe9a673da72e68c95387d3cfa3
Sean Upton <sdupton@gmail.com>
2011-09-22 17:43:42 -0600

    Chart view implementation (view class, template, javascript that makes AJAX call to JSON API URLs, sets up bar and line charts appropriately in chart div placeholder).

---
+++ uu/chart/browser/chart.py
@@ -0,0 +1,12 @@
+
+
+class ChartView(object):
+    """Page using jqPlot to render a chart from AJAX-loaded JSON"""
+    
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def json_url(self):
+        return '%s/%s' % (self.context.absolute_url(), '@@chart_json')
+
---
+++ uu/chart/browser/resources/js/ec5compat.js
@@ -0,0 +1,72 @@
+/** partial ECMAScript 5 compatibility for MSIE<9 and MSIE9+ document
+  * compatibility modes <IE8
+  *
+  * Implemented:
+  *     * Object.keys
+  *     * Array.prototype.reduce
+  *     * Array.prototype.map
+  */
+
+
+// Object.keys -- via moz dev: http://goo.gl/mN0cO
+if(!Object.keys) Object.keys = function(o){
+  if (o !== Object(o))
+    throw new TypeError('Object.keys called on non-object');
+  var ret=[],p;
+  for(p in o) if(Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);
+  return ret;
+}
+
+// Array.prototype.reduce -- via moz dev: http://goo.gl/pF3mW
+if ( !Array.prototype.reduce ) {
+  Array.prototype.reduce = function reduce(accumulator){
+    var i, l = this.length, curr;
+    if(typeof accumulator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+      throw new TypeError("First argument is not callable");
+    if((l == 0 || l === null) && (arguments.length <= 1))// == on purpose to test 0 and false.
+      throw new TypeError("Array length is 0 and no second argument");
+    if(arguments.length <= 1){
+      curr = this[0]; // Increase i to start searching the secondly defined element in the array
+      i = 1; // start accumulating at the second element
+    } else {
+      curr = arguments[1];
+    }
+    for(i = i || 0 ; i < l ; ++i){
+      if(i in this)
+        curr = accumulator.call(undefined, curr, this[i], i, this);
+    }
+    return curr;
+  };
+}
+
+// Array.prototype.map -- via moz dev, algorithm from es5.github.com:
+//   http://goo.gl/cmEVv
+if (!Array.prototype.map) {
+  Array.prototype.map = function(callback, thisArg) {
+    var T, A, k;
+    if (this == null) {
+      throw new TypeError(" this is null or not defined");
+    }   
+    var O = Object(this);
+    var len = O.length >>> 0;
+    if ({}.toString.call(callback) != "[object Function]") {
+      throw new TypeError(callback + " is not a function");
+    }   
+    if (thisArg) {
+      T = thisArg;
+    }   
+    A = new Array(len);
+    k = 0;
+    while(k < len) {
+      var kValue, mappedValue;
+      if (k in O) {
+        kValue = O[ k ];
+        mappedValue = callback.call(T, kValue, k, O); 
+        A[ k ] = mappedValue;
+      }   
+      k++;
+    }   
+    return A;
+  };          
+}
+
