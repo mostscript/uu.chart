288b59b64d17d871b4149176b7c09383a74046c4
Sean Upton <sdupton@gmail.com>
2013-03-18 15:54:20 -0600

    Chart and report UI features, most notably buttons and content listing actions for folks who can manage/add

--- uu/chart/browser/chart.py
+++ uu/chart/browser/chart.py
@@ -1,5 +1,8 @@
 import random
+
 from plone.uuid.interfaces import IUUID
+from zope.component.hooks import getSite
+from zope.security import checkPermission
 
 
 class ChartView(object):
@@ -33,14 +36,43 @@ class ChartView(object):
             cache_bust,
             )
     
-    def divstyle(self, context=None, width=600, height=300):
+    def _fixedheight(self, context):
+        """return fixed height in pixels or None"""
+        height = getattr(context, 'height', None) or height
+        height_units = getattr(context, 'height_units', None)
+        if height_units == 'px':
+            return height
+        return None
+    
+    def can_manage(self):
+        return checkPermission('cmf.ModifyPortalContent', self.context)
+
+    def can_add(self):
+        return checkPermission('cmf.AddPortalContent', self.context)
+    
+    def addable(self, typename):
+        return typename in getSite().portal_types.objectIds()
+    
+    def divstyle(self, context=None, width=100, height=50):
+        """
+        Get width, possibly height styles for chart div.  Note that the
+        height should be set in JavaScript (not here) when the height
+        is specified as anything other than fixed pixels (that is, the
+        JavaScript gets an aspect ratio W:H as array in JSON, and
+        applies it when it is specified -- the JSON API should omit
+        aspect ratio height is specified in pixels).
+        """
         if context is None:
             context = self.context
         width = getattr(context, 'width', None) or width
-        height = getattr(context, 'height', None) or height
-        base = 'width:%spx;height:%spx' % (width, height)
+        width_units = getattr(context, 'width_units', None)
+        height = self._fixedheight(context)
+        if width and not width_units:
+            # backward compatibility for old charts, set to 100%
+            width = 100
+        width_units = width_units or '%'
+        base = 'width:%s%s;' % (width, width_units)
+        if height:
+            base += 'height:%spx;' % (height,)
         return base
-        if width <= 270:
-            return '%s;float:left' % base
-        return '%s;clear:both;' % base
 
