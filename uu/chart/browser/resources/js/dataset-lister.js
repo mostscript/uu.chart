b2e28563a76d672ddae393a3379cb369632628c4
Sean Upton <sdupton@gmail.com>
2013-03-21 13:44:13 -0600

    compatibility mode for browsers that do not support MutationObserver -- uses dumb setInterval every 2 seconds to check, and avoids making duplicate ajax requests for the same URL after selection of a measure.

--- uu/chart/browser/resources/js/dataset-lister.js
+++ uu/chart/browser/resources/js/dataset-lister.js
@@ -4,7 +4,10 @@
     
     var ns = {};
 
+    // global state
     ns.observer_installed = false;
+    ns.last_json_url = '';
+    ns.timer = null;
 
     ns.alltrue = function (seq, callback) {
         var fn = callback || function (v) { return new Boolean(v); },
@@ -16,7 +19,7 @@
         var radio = $(radio),
             path = radio.attr('value'),
             json_url = './@@list_datasets?measure=' + path;
-        if (!json_url) {
+        if (!json_url || json_url == ns.last_json_url) {
             return;
         }
         $.ajax({
@@ -40,6 +43,7 @@
                         option.appendTo(dataset_select);
                     });
                 }
+                ns.last_json_url = json_url;
             }
         });
     };
@@ -47,7 +51,7 @@
     if (MutationObserver) {
 
         // handle change in measure selection via widget DOM change:
-        var install_measure_observer = function () {
+        ns.install_measure_observer = function () {
             var swidget = $('#form-widgets-measure-input-fields')[0],
                 observer;
             if (!swidget) {
@@ -68,40 +72,50 @@
             ns.observer_installed = true;
         };
 
-        $(document).ready(function () {
-            var body = $('body'),
-                add_body_classes = [
-                    'template-uu.chart.data.measureseries',
-                    'portaltype-uu-chart-timeseries'
-                ],
-                is_add_form = ns.alltrue(
-                    add_body_classes,
-                    function (v) { return body.hasClass(v); }
-                ),
-                edit_body_classes = [
-                    'template-edit',
-                    'portaltype-uu-chart-data-measureseries',
-                ],
-                is_edit_form = ns.alltrue(
-                    edit_body_classes,
-                    function (v) { return body.hasClass(v); }
-                ),
-                radio = $('input#form-widgets-measure-0');
-            if (radio.length && is_add_form) {
-                ns.measure_radio_callback(radio);
-            }
-            if (is_edit_form || is_add_form) {
-                console.log('Measure data sequence edit or add form');
-                if (!ns.observer_installed) {
-                    install_measure_observer();
-                }
-            }
-        });
-
     } else {
         // compatibilty, likely MSIE, fallback is setInterval polling :-(
-        console.log('compatibility'); //TODO implement, remove boilerplate
+        ns.install_measure_observer = function () {
+            var wait = null,
+                callback = function () {
+                var radio = $('input#form-widgets-measure-0');
+                if (!radio.length) {
+                    return;
+                }
+                ns.measure_radio_callback(radio);
+            };
+            ns.timer = setInterval(callback, 2000);
+            ns.observer_installed = true;
+        }
     }
+    
+    $(document).ready(function () {
+        var body = $('body'),
+            add_body_classes = [
+                'template-uu.chart.data.measureseries',
+                'portaltype-uu-chart-timeseries'
+            ],
+            is_add_form = ns.alltrue(
+                add_body_classes,
+                function (v) { return body.hasClass(v); }
+            ),
+            edit_body_classes = [
+                'template-edit',
+                'portaltype-uu-chart-data-measureseries',
+            ],
+            is_edit_form = ns.alltrue(
+                edit_body_classes,
+                function (v) { return body.hasClass(v); }
+            ),
+            radio = $('input#form-widgets-measure-0');
+        if (radio.length && is_add_form) {
+            ns.measure_radio_callback(radio);
+        }
+        if (is_edit_form || is_add_form) {
+            if (!ns.observer_installed) {
+                ns.install_measure_observer();
+            }
+        }
+    });
 
 })(jQuery);
 
