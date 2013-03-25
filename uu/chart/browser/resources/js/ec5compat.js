2db5338c5f76f6d85980dda3993c1e45dc504192
Sean Upton <sdupton@gmail.com>
2013-03-25 13:09:47 -0600

    add Array.prototype.forEach to ec5compat.js

--- uu/chart/browser/resources/js/ec5compat.js
+++ uu/chart/browser/resources/js/ec5compat.js
@@ -70,3 +70,11 @@ if (!Array.prototype.map) {
   };          
 }
 
+// Array.prototype.forEach via mozdev: http://goo.gl/GXKkL
+if ( !Array.prototype.forEach ) {
+  Array.prototype.forEach = function(fn, scope) {
+    for(var i = 0, len = this.length; i < len; ++i) {
+      fn.call(scope, this[i], i, this);
+    }
+  }
+}
