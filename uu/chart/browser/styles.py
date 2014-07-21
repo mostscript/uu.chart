9530281ff62591442a29486789a9c22f564adb6f
Sean Upton <sdupton@gmail.com>
2014-07-21 12:59:20 -0600

    unicode for line style creation

--- uu/chart/browser/styles.py
+++ uu/chart/browser/styles.py
@@ -168,7 +168,7 @@ class ReportStylesView(object):
             createContentInContainer(
                 stylebook,
                 LINESTYLE_TYPE,
-                title='Line style %s' % i,
+                title=u'Line style %s' % i,
                 )
         clone_chart_styles(source=mimic, target=stylebook)
         self._contained = self._stylebooks = None  # un-cache now outdated
